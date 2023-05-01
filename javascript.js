157d8170e43adbca423a10a94a679eac77fa406f
Dude-Trust-Me <iankent555@gmail.com>
2023-05-01 21:18:38 +0800

    done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-function
+let choicesRockPaperScissorGame = ['rock','paper','scissors'];
+    
+function getComputerChoice(){
+    let randomArrayNumber = choicesRockPaperScissorGame[Math.floor(Math.random()*choicesRockPaperScissorGame.length)];
+    console.log("Computer : "+randomArrayNumber);
+}
+
+function playRound(playerSelection, computerSelection) {
+    if(playRound("paper","rock")){
+        console.log("Paper beats ROCK!");
+    } else if (playRound('paper','scissor')){
+        console.log('Scissor beats PAPER!');
+    }else if (playRound('rock','scissor')){
+        console.log('Rock beats SCISSOR!');
+    }else if (playRound('scissor','paper')){
+        console.log('Scissor beats PAPER!');
+    }else {
+         console.log("draw");
+    }
+}
+  const playerSelection= "rock";
+  let pSelection = playerSelection.toLowerCase();
+  console.log("Person : "+pSelection);
+  const computerSelection = getComputerChoice();
+
+  console.log(playRound(playerSelection, computerSelection));
